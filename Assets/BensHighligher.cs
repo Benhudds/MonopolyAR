5df5f78de3838e264d4c94b35458e40d5c716e00
Ben Huddleston <benhudds@hotmail.co.uk>
2017-02-13 17:47:59 +0000

    Added highlighting of targeted square
    Corrected camera configuration issue and added highlighting of the
    square that the user is pointing the camera at

---
+++ Assets/BensHighligher.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BensHighligher : MonoBehaviour
+{
+    private List<Transform> tHighlighted = new List<Transform>();
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+        this.tHighlighted.ForEach(t => t.GetComponent<MeshRenderer>().material.color = Color.white);
+
+        tHighlighted.Clear();
+
+	    RaycastHit rhit;
+	    if (Physics.Raycast(new Ray(transform.position, transform.forward), out rhit, 1000))
+	    {
+	        if (rhit.transform.name == "SquareBase" && !tHighlighted.Contains(rhit.transform))
+	        {
+	            rhit.transform.GetComponent<MeshRenderer>().material.color = Color.cyan;
+                tHighlighted.Add((rhit.transform));
+
+            }
+	    }
+	}
+}
